66097091572f9bc1ae9c5c95fda9bd1d9a94887e
jordanmel <jmcatabona0101@gmail.com>
2020-01-07 21:32:40 +0800

    up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 window.onload = function () {
-
     width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    console.log(width);
 }
 
 window.onresize = function () {
     width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-    if (width < 768) {
-        location.reload();
-    }
-
     initSwiper();
-
 }
