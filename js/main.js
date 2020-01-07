05b47ee3206a27d82b11cb68cff761c716ce60ab
jordanmel <jmcatabona0101@gmail.com>
2020-01-07 20:23:24 +0800

    up

--- js/main.js
+++ js/main.js
@@ -9,28 +9,4 @@ window.onresize = function () {
 
     initSwiper();
 
-
-    // var swiperjs1 = document.getElementById('swiperjs1');
-    // var swiperjs2 = document.getElementById('swiperjs2');
-
-    // swiperjs1.remove();
-    // swiperjs2.remove();
-
-
-
-    // var main = document.getElementById('docu-body');
-
-    // var script1 = document.createElement('script');
-    // script1.setAttribute('src', 'https://unpkg.com/swiper/js/swiper.js');
-    // script1.setAttribute('id', 'swiperjs1');
-
-    // var script2 = document.createElement('script');
-
-    // script2.setAttribute('src', 'https://unpkg.com/swiper/js/swiper.min.js');
-    // script2.setAttribute('id', 'swiperjs2');
-
-    // main.append(script1);
-    // main.append(script2);
-
-
 }
